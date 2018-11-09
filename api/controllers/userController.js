ab6811bcdd4c79ee9a0fffe0b58d59e5ab299d0a
PakornPiyaromrat <nyanmarup@gmail.com>
2018-11-09 21:32:38 +0700

    Revert "node mailer"
    This reverts commit 688adfd0ef4d6f46486de8e3d2179dc7a546fe3c.

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -115,40 +115,7 @@ exports.forgot =(req,res,next)=>{
 }
 exports.verify =(req,res,next)=>{
 
-    
 }
-
 exports.resend =(req,res,next)=>{
     
-     // create reusable transporter object using the default SMTP transport
-     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-            user: 'rendezvarify@gmail.com',
-            pass: 'sese59050'
-    },   
-    });
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Rendez Contect" <rendezvarify@gmail.com>', // sender address
-        to: req.body.email, // list of receivers
-        subject: 'Verify Your Email For Rendez', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Link For Verify</b>' // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
 }
