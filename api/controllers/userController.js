edf1d162da991be1e4dbc182adccd9c4c31baea4
Goramda <goddy_2556@hotmail,com>
2018-11-15 22:10:02 +0700

    update 15

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -133,9 +133,45 @@ exports.forgot =(req,res,next)=>{
         .exec()
         .then(user=>{
             if(user.length < 1){
-                return res.status(401).json({
-                    message : "User doesn't found"
+                 res.status(401).json({
+                    message : "Email doesn't found"
                 })
+           }else{
+            
+                            // create reusable transporter object using the default SMTP transport
+                        let transporter = nodemailer.createTransport({
+                            host: 'smtp.gmail.com',
+                            port: 587,
+                            secure: false,
+                            requireTLS: true,
+                            auth: {
+                                user: 'rendezvarify@gmail.com',
+                                pass: 'sese59050'
+                        },   
+                        });
+                        // setup email data with unicode symbols
+                        let mailOptions = {
+                            from: '"Rendez Contect" <rendezvarify@gmail.com>', // sender address
+                            to: req.body.email, // list of receivers
+                            subject: 'Foegot Password', // Subject line
+                            //text: 'Hello world?', // plain text body
+                            html: '<p>'+req.body.password+'</p>' // html body
+                        };
+        
+                        // send mail with defined transport object
+                        transporter.sendMail(mailOptions, (error, info) => {
+                            if (error) {
+                                return console.log(error);
+                            }
+                            console.log('Message sent: %s', info.messageId);
+                            // Preview only available when sending through an Ethereal account
+                            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+                        });
+                        res.status(200).json({
+                            message : "Check Your Email"
+                        })
            }
            
         })
