0225fefbb81c91d2875b07b5a2294e020402fec0
soap51 <soap51@outlook.com>
2018-11-17 20:23:12 +0700

    Remove Bug from korn

--- api/routes/event/private/commentEvent/commentEventRoute.js
+++ api/routes/event/private/commentEvent/commentEventRoute.js
@@ -3,6 +3,5 @@ const router = express.Router()
 const commentEventController = require('../../../../controllers/commentEventController')
 router.post('/:commentID' , commentEventController.getAllEventComment)
 router.post('/' , commentEventController.addcomment)
-router.delete('/:commentID' , commentEventController.deleteComment)
-router.patch('/:commentID' , commentEventController.updateComment)
+
 module.exports = router
