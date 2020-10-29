31345850ca458f91edef6b362a7ad08d8f5e79de
Sarit Soponvattansevi <sarit.s@cdg.co.th>
2020-10-29 12:48:16 +0700

    fix deprecated function

--- api/routes/event/private/eventRoute.js
+++ api/routes/event/private/eventRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const eventController = require('../../../controllers/eventController')
 const commentEventRoute = require('./commentEvent/index')
-router.get("/" ,eventController.getAllEventFeed) // event feed page
+// router.get("/" ,eventController.getAllEventFeed) // event feed page
 router.post("/" , eventController.createEvent) // create event 
 router.patch('/:eventID' , eventController.joinEvent) // join event
 router.get("/:eventID" , eventController.getEventDetail) // get event detail
